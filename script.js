3b92b783c55cb9f497f308057f7ad46bb6878cc7
pratchad17 <pratchad.alavi@gmail.com>
2023-09-15 21:05:38 +0600

    sign updated

--- script.js
+++ script.js
@@ -1,77 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //**************************************************************/
     //********************Sticky Menu*******************************/
-    $(window).scroll(function(){
-        if(this.scrollY>20){
-            $('.navbar').addClass("sticky");
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
         }
-        else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        if(this.scrollY>500){
-            $('.scroll-up-btn').addClass("show");
-        }
-        else{
-            $('.scroll-up-btn').removeClass("show");
+
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
         }
     });
-    
+
     //**************************************************************/
     //********************Slide Up Button***************************/
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
     });
 
     //**************************************************************/
     //********************Toggle Menu*******************************/
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
     });
-
 
     //**************************************************************/
     //********************Owl Carousel******************************/
-    $('.carousel').owlCarousel({
-        margin:20,
+    $(".carousel").owlCarousel({
+        margin: 20,
         loop: true,
-        autoplayTimeOut:2000,
-        autoplayHoverPause:true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
         responsive: {
-            0:{
-                items:1,
-                nav:false
+            0: {
+                items: 1,
+                nav: false,
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false,
             },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-
+            1000: {
+                items: 3,
+                nav: false,
+            },
+        },
     });
-
-
 
     //**************************************************************/
     //********************Typing Effect*****************************/
-    var typed = new Typed(".typing",{
-        strings:["Software Engineer","Software Designer"],
-        typeSpeed:80,
-        backSpeed:40,
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Front-end Engineer"],
+        typeSpeed: 80,
+        backSpeed: 40,
         startDelay: 2500,
-        loop:true
+        loop: true,
     });
 
-    var typed = new Typed(".typing-name",{
-        strings:["Safkat Hasin Alavi"],
-        typeSpeed:120,
-        showCursor: false               
+    var typed = new Typed(".typing-name", {
+        strings: ["Safkat Hasin Alavi"],
+        typeSpeed: 120,
+        showCursor: false,
     });
-
-
 });
